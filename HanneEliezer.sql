5d3aa32b48e621ddacf565427fa4c269668390c3
Eliezer Djihinto <djihintomahugnon@gmail.com>
2026-02-11 16:29:14 +0100

    Question 1 : création des tables regions et departements et population des tables

--- HanneEliezer.sql
+++ HanneEliezer.sql
@@ -1,1 +1,29 @@
 /* Partie 1 du TD */
+
+.mode box
+PRAGMA foreign_keys = on;
+
+DROP TABLE IF EXISTS regions;
+DROP TABLE IF EXISTS departements;
+
+
+CREATE TABLE regions (
+  rid char(5) primary key,
+  nom varchar(50),
+  chefLieu varchar(30)
+);
+
+CREATE TABLE departements (
+  code numeric(2) primary key,
+  nom varchar(30),
+  prefecture varchar(50),
+  rid char(5),
+  foreign key (rid) references regions
+);
+
+.separator ','
+
+.import 'dept-files/regions.csv' regions
+
+.import 'dept-files/departements.csv' departements
+
